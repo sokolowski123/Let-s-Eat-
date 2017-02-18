2272f8a191f6e2751c797b696daddb4384df2e1c
wojtr <rwojtane@purdue.edu>
2017-02-17 19:10:21 -0500

    RESTFUL Server Start
    I started makeing a RESTful Server useing Jersey.

---
+++ LetsEatServer/src/Server/ServerResources.java
@@ -0,0 +1,15 @@
+package Server;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/user")
+public class ServerResources {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getUserName() {
+		return "Name Placeholder"; 
+	}
+	
+}
