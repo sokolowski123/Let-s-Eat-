08c36226c64b5f1376b8d8c32c13e0e5ec5b9725
wojtr <rwojtane@purdue.edu>
2017-02-25 22:23:15 -0500

    Made server run on tomcat 8

---
+++ Lets-Eat-Server-/src/LetsEatServer/Account.java
@@ -0,0 +1,11 @@
+package LetsEatServer;
+
+public class Account {
+	String email;
+	String password;
+	
+	public Account(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+}
---
+++ Lets-Eat-Server-/src/LetsEatServer/AccountResources.java
@@ -0,0 +1,48 @@
+package LetsEatServer;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/account_info")
+public class AccountResources {
+	@GET
+	@Path("/{userId}")
+	public Profile getProfile(@PathParam("userId") String userId) {
+		
+		/* Get account_info of user with the given userId. */
+		
+		return null;
+	}
+	
+	@PUT
+	@Path("/{userId}")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void newProfile(
+			@PathParam("userId") String userId,
+			@FormParam("email") int age,
+			@FormParam("password") int prefMaxAge) {
+		
+		/* Place account_info of the user with the given userId in the database. */
+		
+	}
+	
+	@POST
+	@Path("/{userId}")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void changeProfile(
+			@PathParam("userId") String userId,
+			@FormParam("email") int age,
+			@FormParam("password") int prefMaxAge) {
+		
+		/* Change account_info of the user with the given userId to the given information. */
+		
+	}
+	
+    @DELETE
+    @Path("/{userId}")
+    public void deleteProfile(@PathParam("userId") String userId) {
+    	
+    	/* Delete the account_info of the user with the given userId. */
+    	
+    }
+}
