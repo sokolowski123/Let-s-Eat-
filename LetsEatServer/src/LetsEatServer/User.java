bac00e85dda237b483a4411a066328b6a3552d18
sokolowski123 <sokola@purdue.edu>
2017-03-03 00:56:22 -0500

    Update User.java

--- LetsEatServer/src/LetsEatServer/User.java
+++ LetsEatServer/src/LetsEatServer/User.java
@@ -3,9 +3,11 @@ package LetsEatServer;
 public class User {
 	String dateOfBirth;
 	String userId;
-    String name;
-    String gender;
-    Match[] matches;
+        String name;
+    	String gender;
+    	Match[] matches;
+  	int[] valid;
+	int[] blocked;
     
     public User(String dateOfBirth, String userId, String name, String gender, Match[] matches) {
     	this.dateOfBirth = dateOfBirth;
