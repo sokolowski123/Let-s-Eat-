00d3766e6b721056a9984e2ffae5a11264b3b2c7
sokolowski123 <sokola@purdue.edu>
2017-03-03 12:29:26 -0500

    Update ResponseParser.java

--- LetsEatServer/src/LetsEatServer/ResponseParser.java
+++ LetsEatServer/src/LetsEatServer/ResponseParser.java
@@ -8,6 +8,28 @@ import com.mongodb.*;
 
 public class ResponseParser {
 	
+	public static void main(String[] args) {
+		
+		ResponseParser parse = new ResponseParser();
+		Document user = new Document();
+		user.append("_id", "sokola@purdue.edu");
+		user.append("_pass", "43dfdf3476gede3");
+		user.append("_matches", "[user1@purdue.edu, user2@purdue.edu, user3@purdue.edu]");
+		user.append("_chat", "[[0412170916sokola@purdue.edu..hey!, 0412170916user1@purdue.edu..how's it going?, 0412170916sokola@purdue.edu..nbu?],[user1@purdue.edu..hey],"
+				+ "[sokola@purdue.edu: where you trying to eat?]]");
+		user.append("blocked", "[0, 0, 0]");
+		user.append("isValid", "[1, 1, 0]");
+		user.append("dob", "5/29/1996");
+		user.append("name", "Alex Sokol");
+		user.append("userId", "sokola");
+		user.append("gender", "male");
+		
+		User userA = parse.parseUser(user);
+		parse.parseMessage(user);
+		
+	}
+	
+	
 	Account parseAccount(Document user) {
 		String email = user.get("_id").toString();
 		String pass = user.get("_pass").toString();
