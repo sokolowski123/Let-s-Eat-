552f21b44d707bb9f99f5aaad18d31617a8ed043
sokolowski123 <sokola@purdue.edu>
2017-03-24 12:25:29 -0400

    Update Search.java

--- LetsEatServer/src/LetsEatServer/Search.java
+++ LetsEatServer/src/LetsEatServer/Search.java
@@ -3,8 +3,8 @@ package LetsEatServer;
 public class Search {
 
 	public static double getDistance(double[] user1, double[] user2) {
-	  Location loc1 = new Location("Purdue", user1[0],user1[1]);
-		Location loc2 = new Location("Lucas Oil", user2[0],user2[1]);
+	  Location loc1 = new Location("", user1[0],user1[1]);
+		Location loc2 = new Location("", user2[0],user2[1]);
 		double distance = loc1.distanceTo(loc2);
 		return distance;
 	}
