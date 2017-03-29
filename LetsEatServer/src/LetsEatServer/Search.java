28ee3794bbb306dd14362037b2657106f8512fd3
wojtr <rwojtane@purdue.edu>
2017-03-29 11:51:01 -0400

    Search function and new User Put and Post now work.

--- LetsEatServer/src/LetsEatServer/Search.java
+++ LetsEatServer/src/LetsEatServer/Search.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class Search {
 
 	public static double getDistance(List<Double> user1, List<Double> user2) {
-		Location loc1 = new Location("", Double.parseDouble(user1.get(0).toString()), Double.parseDouble(user1.get(0).toString()));
-		Location loc2 = new Location("", Double.parseDouble(user2.get(0).toString()), Double.parseDouble(user2.get(0).toString()));
+		Location loc1 = new Location("", user1.get(0).doubleValue(), user1.get(0).doubleValue());
+		Location loc2 = new Location("", user2.get(0).doubleValue(), user2.get(0).doubleValue());
 		double distance = loc1.distanceTo(loc2);
 		return distance;
 	}
--- LetsEatServer/src/LetsEatServer/SearchResources.java
+++ LetsEatServer/src/LetsEatServer/SearchResources.java
@@ -13,24 +13,41 @@ public class SearchResources {
 	@GET
     @Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
+	public String getSearch(@PathParam ("userId") String userId, @PathParam ("age") int age) {
 		
+		/* Connect to the mongoDb database. */
 		ServerAddress adr = new ServerAddress("ec2-52-41-45-85.us-west-2.compute.amazonaws.com", 27017);
 		MongoClient client = new MongoClient(adr);
 		MongoDatabase database = client.getDatabase("Users");
 		MongoCollection<Document> collection = database.getCollection("USERS");
+		
+		/* Find all the users at the given age. */
 		FindIterable<Document> users = collection.find(new Document("age", age));
+		
+		/* Create the document that while contain all the users at the given age. */
 		Document doc = new Document();
-		MongoCursor<Document> cursor = users.iterator();
+		
+		/* Find the user that is calling for the search. */
 		Document caller = collection.find(new Document("_id", userId)).first(); 
-		//while (cursor.hasNext()) {
+		
+		/* Iterate through the users with a given age. */
+		MongoCursor<Document> cursor = users.iterator();
+		while (cursor.hasNext()) {
 			Document user =  cursor.next();
-			return callerLoc.get(0);
-			/* if (Search.getDistance(callerLoc, userLoc) <= ((double) user.get("maxRange") + (double) caller.get("maxRange"))) {
+			
+			/* Only return the users who are within the calling user's range. */
+			List<Double> callerLoc = (List<Double>) caller.get("location");
+			List<Double> userLoc = (List<Double>) user.get("location");
+			if (Search.getDistance(callerLoc, userLoc) <= Double.parseDouble(Integer.toString(((int) user.get("maxRange") + (int) caller.get("maxRange"))))) {
 				doc.append(user.getString("_id"), user);
 			}
+			
 		}
+		
+		/* Close connection to database. */
 		client.close();
-		return doc.toJson(); */
+		
+		return doc.toJson();
 		
 	}
 
